ab1ad7c69464a8791544363503e713c1d0d96803
Maximiliano Ricagno <ricagnom@gmail.com>
2017-02-04 00:44:22 -0300

    Encapsulamiento stack

--- EjLibro/TP 02 - Ej 1.24/Stack.c
+++ EjLibro/TP 02 - Ej 1.24/Stack.c
@@ -10,29 +10,54 @@
 #include "Stack.h"
 #include <stdio.h>
 
+struct {
+    int top;
+    char character[MAX_STACK];
+} stk;
+//DEFINICION DE FUNCIONES INVOCADAS
 //Stack
-int push(stack *stk, char chr)
+int push(char chr)
 {
-    if (stk->top == MAX_STACK) {
+    if (stk.top == MAX_STACK) {
         printf("Stack overflow. No es posible poner mas valores en la pila.\n");
         return EXIT_FAILURE;
     }
-    stk->top++;
-    stk->character[stk->top] = chr;
+    stk.top++;
+    stk.character[stk.top] = chr;
     
     return 0;
 }
 
 //Pop
-char pop(stack *stk)
+char pop()
 {
     char retValue;
     
-    if (stk->top == 0) {
+    if (stk.top == 0) {
         return 1;
     }
-    retValue = stk->character[stk->top];
-    stk->top--;
+    retValue = stk.character[stk.top];
+    stk.top--;
     
     return (retValue);
 }
+
+void fin_del_programa_por_mal_cierre(){
+    system("cls");
+    printf("El programa vuelve a iniciar. Se limpia la pila\n");
+    stk.top = 0;
+};
+
+int isEmpty (void)
+{
+    if (stk.top == 0 ){
+        return 1;}
+    else{
+        return 0;
+        }
+};
+
+int get_top(void)
+{
+    return stk.top;
+};

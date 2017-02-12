fd0007efeb0c66b6d453caea1389a0304ea146c9
Maximiliano Ricagno <ricagnom@gmail.com>
2017-02-12 17:25:24 -0300

    Actualización a última versión

--- EjLibro/TP 01 - Ej 1.23/main.c
+++ EjLibro/TP 01 - Ej 1.23/main.c
@@ -13,75 +13,43 @@
 
 int main()
 {
-    int len, input_char, function_return;
-    char input_array[MAXLENGTH];
+    int ch;
     enum states state;
-    bool finish_program;
+
+    state=NO_COMMENT; //Estado de comienzo
     
-    finish_program=false;
-    
-    while(finish_program==false)
-    {
-        puts("--------------------------------------------------");
-        puts("Ingresar el codigo. Apretar Ctrl+D para finalizar:\n");
-        empty_array_for_new_program(input_array);
-        input_char=0;
-        state=NO_COMMENT; //Estado de comienzo
-        if((len=get_line(input_array,MAXLENGTH))>0)
-        {
-            while(input_array[input_char]!='\0')
+            while ((ch = getchar()) != EOF)
             {
-                function_return=0;
                 switch(state)
                 {
                     case NO_COMMENT:
-                        state = read_char_in_nc_state(input_array[input_char], state);
+                        state = read_char_in_nc_state(ch, state);
                         break;
-                    case ESCAPE_NO_COMMENT:
-                        putchar(input_array[input_char]);
-                        state=NO_COMMENT;
+                    case FIRST_CHAR:
+                        state = read_char_in_first_char_state(ch, state);
                         break;
                     case CHAR:
-                        state = read_char_in_char_state(input_array[input_char], state);
+                        state = read_char_in_char_state(ch, state);
                         break;
-                    case ESCAPE_CHAR:
-                        putchar(input_array[input_char]);
-                        state=END_OF_CHAR;
-                        break;
-                    case END_OF_CHAR:
-                        state = read_char_in_end_of_char_state(input_array[input_char],state);
-                        //putchar(input_array[input_char]);
-                        break;
-                    case EMPTY_CHAR_ERROR:
-                        goto END_OF_STATE_MACHINE;
-                    case MORE_THAN_ONE_CHAR_ERROR:
-                        goto END_OF_STATE_MACHINE;
                     case STRING:
-                        state = read_char_in_string_state(input_array[input_char], state);
-                        putchar(input_array[input_char]);
-                        break;
-                    case ESCAPE_STRING:
-                        putchar(input_array[input_char]);
-                        state=STRING;
+                        state = read_char_in_string_state(ch, state);
+                        putchar(ch);
                         break;
                     case POSSIBLE_COMMENT:
-                        state = read_char_in_pc_state(input_array[input_char],state);
+                        state = read_char_in_pc_state(ch,state);
                         break;
                     case LINE_COMMENT:
-                        state = read_char_in_lc_state(input_array[input_char],state);
+                        state = read_char_in_lc_state(ch,state);
                         break;
                     case COMMENT:
-                        state = read_char_in_comment_state(input_array[input_char], state);
+                        state = read_char_in_comment_state(ch, state);
                         break;
                     case POSSIBLE_END_OF_COMMENT:
-                        state = read_char_in_peoc_state(input_array[input_char],state);
+                        state = read_char_in_peoc_state(ch,state);
+                        break;
                 }
-                input_char++;	//Para poder trabajar con el siguiente caracter en la proxima iteracion
             }
-        }
-    END_OF_STATE_MACHINE: error_message(state);
-        finish_program = run_program_again();
-    }
+    //Se evaluan posibles errores
+    error_message(state);
     return 0;
-}	//Fin de la FUNCION MAIN
-
+}//Fin MAIN
